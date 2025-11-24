b731a4f43c5d4bf8a100e308dea03b6698955be0
wgff0012-dev <wgff.0012@gmail.com>
2025-11-24 17:44:46 -0300

    Add files via upload

---
+++ Admin/agenda/agendamento.php
@@ -0,0 +1,14 @@
+<h1>Cadastro do agendamento</h1>
+<form action="?pg=admin/agenda/agen-cadastro" method="post">
+    <label>Nome</label>
+    <input type="text" name="nome" placeholder="Seu nome"><br>
+    <label>telefone</label>
+    <input type="text" name="telefone" placeholder="Telefone"><br>
+    <label>data</label>
+    <input type="date" name="data1" ><br>
+    <label>hora</label>
+    <input type="time" name="hora">
+
+    <input type="submit" value="Agendar">
+
+</form>
