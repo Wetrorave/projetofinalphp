bb19fe60faad0f5f706eac8c22702e74a17d785d
neryn <103009671+Wetrorave@users.noreply.github.com>
2025-11-27 20:20:09 -0300

    Add files via upload

--- servicoseprecos.php
+++ servicoseprecos.php
@@ -9,13 +9,15 @@
 <div>
     <h2>Promoções</h2>
     <p>Todas as Promoções Atuais</p>
-    <div class="container mt-3">
+    <div>
         <?php
             while($dados = mysqli_fetch_array($resultado)){
         ?>
+        <div class="container mt-3">
             <h4><?=$dados['nome']?></h4>
             <p><s><?=$dados['preco']?></s></p>
             <h3><?=$dados['precocompromo']?></h3>
+        </div>
         <?php
             }?>
     </div>
@@ -23,13 +25,14 @@
 <div>
     <h2>Produtos e Serviços</h2>
     <p>Lista de Produtos e Serviços Oferecidos</p>
-    <div class="container mt-3">
+    <div>
         <?php
             while($dados1 = mysqli_fetch_array($resultado1)){
         ?>
+        <div class="container mt-3">
             <h3><?=$dados1['nome']?> - <?=$dados1['preco']?></h3>
             <p><?=$dados1['descricao']?></p>
-
+        </div>
         <?php
             }
         ?>
