7f399a9db8d30c23ca9a852aa9aa2569f2b6e2a0
neryn <103009671+Wetrorave@users.noreply.github.com>
2025-12-04 16:31:56 -0300

    Create agen-excluir.php

---
+++ agenda/agen-excluir.php
@@ -0,0 +1,17 @@
+<?php
+
+    require_once "config.inc.php";
+
+    $id = $_GET['id'];
+
+    $sql = "DELETE FROM agendamentos WHERE id = $id";
+
+    $resultado = mysqli_query($conexao, $sql);
+
+    if($resultado){
+        echo "<h2>Agendamento excluído com sucesso.</h2>";
+        echo "<a href='?pg=agenda/agendamento'>Voltar</a>";
+    }else{
+        echo "<h2>Erro ao excluir o agenfamento.</h2>";
+        echo "<a href='?pg=agenda/agendamento'>Voltar</a>";
+    }
